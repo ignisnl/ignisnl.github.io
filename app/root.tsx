08a1fcde5cc997eaa7dbb6905301c1dc19cb1223
Lorenzo Grillo <lgrillo0413@gmail.com>
2024-12-17 23:45:07 +1100

    add files and scripts to allow for SPA fallback on GitHub Pages' static hosting service

--- app/root.tsx
+++ app/root.tsx
@@ -32,6 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script src="/spa_redirect.js" />
       </head>
       <body>
         {children}
